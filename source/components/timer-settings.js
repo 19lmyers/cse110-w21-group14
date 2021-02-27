ef9b42b905873a42a7ba7e0328dd2d14c68e302c
Luke Myers <19lmyers@gmail.com>
2021-02-27 14:47:20 -0800

    Use --fix on ESLint to fix most of the errors

--- source/components/timer-settings.js
+++ source/components/timer-settings.js
@@ -1,10 +1,10 @@
 class TimerSettingsControl extends HTMLElement {
 
     constructor() {
-        console.log("settings");
+        console.log('settings');
         super();
 
-        this.attachShadow({ mode: 'open' });
+        this.attachShadow({mode: 'open'});
         
     } /* constructor */
-} 
+}
--- test/sum.js
+++ test/sum.js
@@ -1,9 +1,9 @@
 /**
  * @param {number} a
  * @param {number} b
- * @returns {number}
+ * @return {number}
  */
 function sum(a, b) {
-  return a + b;
+    return a + b;
 }
 module.exports = sum;
