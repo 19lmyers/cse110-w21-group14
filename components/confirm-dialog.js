8e015a1a989390973d92f724637067b78db104f7
moreramonn <moreramonn@users.noreply.github.com>
2021-02-28 06:19:44 +0000

    deploy: 826e6e13d673e8dda8d82f62ef6297a75e4fe407

---
+++ components/confirm-dialog.js
@@ -0,0 +1,23 @@
+class ConfirmDialog extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.querySelector('#confirm-dialog-template').content;
+
+    const shadowRoot = this.attachShadow({mode: 'open'})
+      .appendChild(template.cloneNode(true));
+
+    let shadow = this.shadowRoot;
+
+    shadow.querySelector('.confirm-button').addEventListener('click', () => {
+      this.dispatchEvent(new Event('confirmPressed'));
+      this.remove();
+    });
+
+    shadow.querySelector('.cancel-button').addEventListener('click', () => {
+      this.remove();
+    });
+  }
+}
+
+customElements.define('confirm-dialog', ConfirmDialog);
